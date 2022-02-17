c61c28d46eca56cf2b9e9db1f9ee7a4b8c85870a
Alex Juarez <karmatr0n.dixit@gmail.com>
2022-02-16 22:52:50 -0600

    Add day2b

---
+++ adventcode/day2b/target/rls/debug/deps/day2b-aa3f5da69c95d85c.d
@@ -0,0 +1,5 @@
+/Users/karmatr0n/src/rust-fu/adventcode/day2b/target/rls/debug/deps/day2b-aa3f5da69c95d85c.rmeta: src/main.rs
+
+/Users/karmatr0n/src/rust-fu/adventcode/day2b/target/rls/debug/deps/day2b-aa3f5da69c95d85c.d: src/main.rs
+
+src/main.rs:
---
+++ adventcode/day2b/target/rls/debug/deps/day2b-afbe1aa7013051a2.d
@@ -0,0 +1,5 @@
+/Users/karmatr0n/src/rust-fu/adventcode/day2b/target/rls/debug/deps/day2b-afbe1aa7013051a2.rmeta: src/main.rs
+
+/Users/karmatr0n/src/rust-fu/adventcode/day2b/target/rls/debug/deps/day2b-afbe1aa7013051a2.d: src/main.rs
+
+src/main.rs:
